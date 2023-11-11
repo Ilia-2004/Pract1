cd42847c164fabf336ee66778dcb8a8c6ee7dcc1
IlyaKrass <36201691+Ilia-2004@users.noreply.github.com>
2023-11-11 23:03:44 +0400

    Initial commit

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,216 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Media;
+using WpfApp1;
+
+namespace inputValidation
+{
+    public partial class MainWindow : Window
+    {
+        private string _file = "employee.txt";
+        private Window1 _window = new Window1();
+
+        public MainWindow() => InitializeComponent();
+
+        // function of the button
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string textID = Identifier.Text;
+            string textSurname = Surname.Text;
+            string textName = Name.Text;
+            string textPatronymic = Patronymic.Text;
+            string textPassport = Passport.Text;
+            string textPhone = MobPhone.Text;
+            string textEmail = Email.Text;
+
+            if (string.IsNullOrWhiteSpace(textID) ||
+                string.IsNullOrWhiteSpace(textName) ||
+                string.IsNullOrWhiteSpace(textSurname) ||
+                string.IsNullOrWhiteSpace(textPatronymic) ||
+                string.IsNullOrWhiteSpace(textPassport) ||
+                string.IsNullOrWhiteSpace(textPhone) ||
+                string.IsNullOrWhiteSpace(textEmail))
+            {
+                if (string.IsNullOrWhiteSpace(textID))
+                    Identifier.BorderBrush = Brushes.Red;
+                if (string.IsNullOrWhiteSpace(textName))
+                    Name.BorderBrush = Brushes.Red;
+                if (string.IsNullOrWhiteSpace(textSurname))
+                    Surname.BorderBrush = Brushes.Red;
+                if (string.IsNullOrWhiteSpace(textPatronymic))
+                    Patronymic.BorderBrush = Brushes.Red;
+                if (string.IsNullOrWhiteSpace(textPassport))
+                    Passport.BorderBrush = Brushes.Red;
+                if (string.IsNullOrWhiteSpace(textPhone))
+                    MobPhone.BorderBrush = Brushes.Red;
+                if (string.IsNullOrWhiteSpace(textEmail))
+                    Email.BorderBrush = Brushes.Red;
+            }
+            else
+            {
+                if (_checkID(textID) &&
+                    _checkNSP(textName, textSurname, textPatronymic) &&
+                    _checkPasport(textPassport) &&
+                    _checkPhone(textPhone) &&
+                    _checkEmail(textEmail))
+                {
+                    File.AppendAllText(_file, "ID " + textID + " ");
+                    Identifier.BorderBrush = Brushes.Gray;
+                    Identifier.Text = null;
+                    File.AppendAllText(_file, "Surname " + textSurname + " ");
+                    Surname.BorderBrush = Brushes.Gray;
+                    Surname.Text = null;
+                    File.AppendAllText(_file, "Name " + textName + " ");
+                    Name.BorderBrush = Brushes.Gray;
+                    Name.Text = null;
+                    File.AppendAllText(_file, "Patronymic " + textPatronymic + " ");
+                    Patronymic.BorderBrush = Brushes.Gray;
+                    Patronymic.Text = null;
+                    File.AppendAllText(_file, "Passport " + textPassport + " ");
+                    Passport.BorderBrush = Brushes.Gray;
+                    Passport.Text = null;
+                    File.AppendAllText(_file, "Phone " + textPhone + " ");
+                    MobPhone.BorderBrush = Brushes.Gray;
+                    MobPhone.Text = null;
+                    File.AppendAllText(_file, "Email " + textEmail);
+                    Email.BorderBrush = Brushes.Gray;
+                    Email.Text = null;
+                    File.AppendAllText(_file, "\n");
+                    MessageBox.Show("Данные сохранены", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    if (!_checkID(textID))
+                        Identifier.BorderBrush = Brushes.Red;
+                    if (!_checkPasport(textPassport))
+                        Passport.BorderBrush = Brushes.Red;
+                    if (!_checkPhone(textPhone))
+                        MobPhone.BorderBrush = Brushes.Red;
+                    if (!_checkEmail(textEmail))
+                        Email.BorderBrush = Brushes.Red;
+                    if (!_checkNSP(textName, textSurname, textPatronymic))
+                    {
+                        Name.BorderBrush = Brushes.Red;
+                        Surname.BorderBrush = Brushes.Red;
+                        Patronymic.BorderBrush = Brushes.Red;
+                    }
+                }
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            _window.Show();
+            Close();
+        }
+
+        // functions used
+        // mail check 
+        private bool _checkEmail(string email)
+        {
+            if (!email.Contains("@"))
+                return false;
+
+            string[] delimiter = email.Split('@');
+            if (delimiter.Length != 2)
+                return false;
+
+            string login = delimiter[0];
+            string domain = delimiter[1];
+
+            if (login.Length == 0 || domain.Length == 0)
+                return false;
+
+            char firstSymbol = login[0];
+            if (Regex.IsMatch(firstSymbol.ToString(), @"[A-Z][a-z]"))
+                return false;
+
+            string[] arrDobain = domain.Split('.');
+            if (arrDobain.Length != 2)
+                return false;
+
+            return true;
+        }
+
+        // phone check
+        private bool _checkPhone(string phone)
+        {
+            if (Regex.IsMatch(phone.ToString(), @"[a-z]!@#\/") || Regex.IsMatch(phone.ToString(), @"[A-Z]!@#\/"))
+                return false;
+
+            switch (phone[0])
+            {
+                case '8':
+                    if (phone.Length != 11)
+                        return false;
+                    break;
+                case '+':
+                    if (phone.Length != 12 && phone[1] != '7')
+                        return false;
+                    break;
+                default:
+                    return false;
+            }
+
+            return true;
+        }
+
+        // pasport check
+        private bool _checkPasport(string pasport)
+        {
+            if (!Regex.IsMatch(pasport.ToString(), @"[0-9]"))
+                return false;
+
+            string[] arrPasport = pasport.Split(' '); ;
+            if (arrPasport.Length != 2)
+                return false;
+
+            string series = arrPasport[0];
+            if (series.Length != 4)
+                return false;
+
+            string number = arrPasport[1];
+            if (number.Length != 6)
+                return false;
+
+            return true;
+        }
+
+        // identity check
+        private bool _checkID(string id)
+        {
+            if (!(Regex.IsMatch(id.ToString(), @"[0-9]")))
+                return false;
+
+            string[] fileContains = File.ReadAllLines(_file);
+            foreach (string line in fileContains)
+            {
+                if (line.Split()[0] == "ID")
+                {
+                    if (line.Split()[1] == id)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        // checking the name, surname and patronymic
+        private bool _checkNSP(string name, string surname, string patronymic)
+        {
+            if ((!Regex.IsMatch(name.ToString(), @"[А-Я]") || !Regex.IsMatch(name.ToString(), @"[а-я]")) ||
+                (!Regex.IsMatch(surname.ToString(), @"[А-Я]") || !Regex.IsMatch(surname.ToString(), @"[а-я]")) ||
+                (!Regex.IsMatch(patronymic.ToString(), @"[А-Я]") || !Regex.IsMatch(patronymic.ToString(), @"[а-я]")))
+                return false;
+
+            if (!(name[0] == name.ToUpper()[0]) ||
+                !(surname[0] == surname.ToUpper()[0]) ||
+                !(patronymic[0] == patronymic.ToUpper()[0]))
+                return false;
+
+            return true;
+        }
+
+    }
+}
---
+++ WpfApp1/Window1.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+using System.Threading; // погуглить, что это за блиблиотека 
+using inputValidation;
+
+namespace WpfApp1
+{
+    public partial class Window1 : Window
+    {
+        private CancellationTokenSource _isAuthorizationWindowOpened = new CancellationTokenSource(); // погуглить тип данных 
+        private DispatcherTimer _timer = new DispatcherTimer();
+        private MainWindow _mainWindow = new MainWindow();
+        private DateTime _time;
+        private string _lineKey = "employee";
+        private int _counter = 0;
+        private int _seconds = 0;
+
+        public Window1()
+        {
+            InitializeComponent();
+            _timer.Interval = new TimeSpan(0, 0, 1);
+            _timer.Start();
+            _timer.Tick += new EventHandler(_timerTick);
+        }
+
+        // function of the button
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string textLogin = Login.Text;
+            string textPassword = Password.Password;
+
+            if (_showTimeMessage() == true)
+            {
+                _timer.Stop();
+                _checkLoginPassword();
+            }  
+        }
+
+        // function of timer
+        private void _timerTick(object sender, EventArgs e)
+        {
+            _seconds++;
+            if (_seconds == 10)
+            {
+                var message = MessageBox.Show($"Уже прошла минута, а вы не ввели данные.\nЗакрыть программу?", "Привышение ожидания", MessageBoxButton.OKCancel);
+                if (message == MessageBoxResult.OK)
+                {
+                    //_timer.Stop();
+                    Close();
+                }
+                _seconds = 0;
+            }
+        }
+
+        // function of chek login and password 
+        private void _checkLoginPassword()
+        {
+            if (Login.Text == _lineKey && Password.Password == _lineKey)
+            {
+                _mainWindow.Show();
+                Close();
+            }
+            else
+            {
+                if (Login.Text != _lineKey)
+                    Login.BorderBrush = Brushes.Red;
+                if (Password.Password != _lineKey)
+                    Password.BorderBrush = Brushes.Red;
+            }
+        }
+
+        // displays a message about the expiration of time
+        public bool _showTimeMessage()
+        {
+            if (_counter == 3)
+            {
+                TimeSpan time = DateTime.Now - _time;
+                if (time.TotalSeconds < 60)
+                {
+                    MessageBox.Show($"Осталось {60 - (int)time.TotalSeconds}");
+                    return false;
+                }
+
+                _counter = 0;
+            }
+            if (Login.Text.Trim() == _lineKey || Password.Password.Trim() == _lineKey)
+            {
+                _counter = 0;
+                return true;
+            }
+
+            _counter = (_counter + 1) % 4;
+            if (_counter == 3)
+            {
+                _time = DateTime.Now;
+                MessageBox.Show("Неправильные данные", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
+// перенести все переменные вначало проекта 
+// настроить кнопку крестик
